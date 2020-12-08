9f67a1ddeef1be60af4e06b2b3d2279557275aad
Jason Molina <70608137+jmolina1474@users.noreply.github.com>
2020-12-07 20:32:18 -0800

    The test run of the final project
    Added Home page.
    Added About page.
    Added Blog page.
    Added Donations page.
    Added placeholder image.
    Added CSS stylesheet.
    Added Javascript to page.

---
+++ Final-Brief/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+    $("#contactbtn").click(function(){
+        $("#contact, #contactbtn").toggle();
+    })
+
+    $('form').submit(function(event){ event.preventDefault(); });
+
+    $("#donatebtn").click(function(){
+        window.alert("Donation Sent, You may leave this page now.");
+    })
+
+    $("#contact-btn").click(function(){
+        window.alert("Message Sent Successfuly");
+    })
+
+    $("#item1").click(function(){
+       $("#desc1, #item1").toggle();
+    })
+
+    $("#desc1").click(function(){
+        $("#desc1, #item1").toggle();
+    })
+
+    $("#item2").click(function(){
+        $("#desc2, #item2").toggle();
+    })
+ 
+    $("#desc2").click(function(){
+        $("#desc2, #item2").toggle();
+    })
+
+    $("#item3").click(function(){
+        $("#desc3, #item3").toggle();
+     })
+ 
+    $("#desc3").click(function(){
+        $("#desc3, #item3").toggle();
+    })
+
+    $("#item4").click(function(){
+        $("#desc4, #item4").toggle();
+     })
+ 
+    $("#desc4").click(function(){
+        $("#desc4, #item4").toggle();
+    })
+
+    $("#item5").click(function(){
+        $("#desc5, #item5").toggle();
+     })
+ 
+    $("#desc5").click(function(){
+        $("#desc5, #item5").toggle();
+    })
+
+    $("#item6").click(function(){
+        $("#desc6, #item6").toggle();
+     })
+ 
+    $("#desc6").click(function(){
+        $("#desc6, #item6").toggle();
+    })
+
+    $("#item7").click(function(){
+        $("#desc7, #item7").toggle();
+     })
+ 
+    $("#desc7").click(function(){
+        $("#desc7, #item7").toggle();
+    })
+
+    $("#item8").click(function(){
+        $("#desc8, #item8").toggle();
+     })
+ 
+    $("#desc8").click(function(){
+        $("#desc8, #item8").toggle();
+    })
+
+    $("#item9").click(function(){
+        $("#desc9, #item9").toggle();
+     })
+ 
+    $("#desc9").click(function(){
+        $("#desc9, #item9").toggle();
+    })
+
+});
